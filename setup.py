1fb5ab014c55bbc425510ae65951ea270d7ecdbf
Jacco Taal <jacco@bitnomica.com>
2013-10-28 15:05:14 +0100

    turmeric now uses gitversion

--- setup.py
+++ setup.py
@@ -2,13 +2,22 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
+try:
+    import gitversion
+    version = gitversion.get_git_version()
+except:
+    try:
+        version = open("RELEASE-VERSION").read()
+    except:
+        version = "dev"
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
     
 setup(
     name = "turmeric",
-    version = "1.1",
+    version = version,
     author = "Jacco Taal",
     author_email = "jacco@bitnomica.com",
     description = ("""Turmeric is  database management tool to
@@ -22,9 +31,16 @@ setup(
     packages=find_packages(exclude = ["test"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["sqlalchemy>0.8" , "psycopg2" ,"babel"],
+    install_requires=["sqlalchemy>0.8",
+                      "psycopg2",
+                      "babel",
+                      "gitversion",
+                  ],
     tests_require=[],
-    setup_requires = [ "setuptools-git>=0.3"],
+    setup_requires = [ "setuptools-git>=0.3",
+                   ],
+    dependency_links = [ "git+ssh://git@gitlab.bitnomica.com/jacco/gitversion.git#egg=gitversion-0.2",
+                     ],
     test_suite="lifeshare",
     entry_points = """\
       [console_scripts]
